1c8bb5e3e943835686a3b5fabb8b8ce73f887a7d
flbr <benrhammond@gmail.com>
2022-01-12 15:47:26 -0800

    new rectangle generation

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
 rectangles = {}
-numRects = 5
+numRects = 7
+rectWidth = 20
+rectSpacing = 30
+rectY = 400
 
 function love.load(args)
   for i = 1, numRects do
@@ -8,34 +11,45 @@ function love.load(args)
     table.insert(rectangles, newRect)
   end
 
-  rectIsDrawn = 0
-  posX = 100
-  point = {
-    x = 250,
-    y = 250,
-  }
+  -- point = {
+    -- x = 250,
+    -- y = 250,
+  -- }
 end
 
 function love.draw()
-  for i = 1, 5 do
-
-    local height = love.math.random(1, 200)
-    posX = posX + 0.1
-
-    love.graphics.line(0, 250, 500, 250)
-    love.graphics.setLineWidth(2)
-    love.graphics.rectangle("line", posX, 250 - height, 20, height) --height measure places y coord on bottom-left corner
-    love.graphics.setLineWidth(2)
-
-    if posX == 200 then
-      break
-    end
+  love.graphics.setLineWidth(2)
+  love.graphics.line(0, rectY, 500, rectY)
+  
+  -- the width of the rectanlges with their spacing
+  rectBlockWidth = ((rectWidth + rectSpacing) * #rectangles) - rectSpacing
+  -- the first x coordinate given the width
+  initialX = (love.graphics.getWidth() - rectBlockWidth) / 2
+  
+  for i = 1, #rectangles do
+    local rect = rectangles[i]
+    local x = initialX + (i-1) * (rectWidth + rectSpacing)
+    love.graphics.rectangle("line", x, rectY - rect.h, rectWidth, rect.h)
   end
 end
 
 
+-- remakes heights
+function newHeights()
+  rectangles = {}
+  for i = 1, numRects do
+    local newRect = {}
+    newRect.h = love.math.random(1, 200)
+    table.insert(rectangles, newRect)
+  end
+end
+
+
+-- handles keypresses
 function love.keypressed(key)
   if key == "escape" then
     love.event.quit(0)
+  elseif key == "space" then
+    newHeights()
   end
 end
